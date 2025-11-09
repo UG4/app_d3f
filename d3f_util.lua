16234094e7517ad740dabad5bf89b7bd2ac21421
Arne Naegel <naegel@gcsc.uni-frankfurt.de>
2025-11-09 21:52:27 +0100

    Adding d3f_util (dummy)

---
+++ d3f_util.lua
@@ -0,0 +1,5 @@
+
+-- For compability reasons, include ug_util only
+-- the old d3f_util.lua file will be included by ug_util
+-- (it is located in plugins/d3f/lua/d3f_util.lua)
+ug_load_script("ug_util.lua") 
